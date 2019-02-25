a2f9f646596b0b269ff794d0b80f368899076601
Juliana Mota <julianamota89@gmail.com>
2019-02-25 23:19:19 +0100

    html and css copy and js start new array done

--- studentlist.js
+++ studentlist.js
@@ -1,4 +1,13 @@
 "use strict";
+const baseLink = "http://petlatkea.dk/2019/students1991.json";
+const studentObject = {
+  fullname: "-student name-",
+  firstname: "-student first name-",
+  lastname: "-student last name-",
+  image: "-student image-",
+  house: "-student house-"
+};
+const arrayOfStudents = [];
 
 window.addEventListener("DOMContentLoaded", init);
 
@@ -11,27 +20,47 @@ function init() {
 
 function getJSON() {
   console.log("getJSON");
-
+  fetch(baseLink)
+    .then(pro => pro.json())
+    .then(makeObject);
   // NOTE: Maybe also call sortByFirst the first time ... Investigate!
-  filterList();
+  //filterList();
+}
+function makeObject(studentList) {
+  //console.log(studentList);
+  studentList.forEach(stuData => {
+    const newStuObject = Object.create(studentObject);
+    const firstSpace = stuData.fullname.indexOf(" ");
+    const lastSpace = stuData.fullname.lastIndexOf(" ");
+    //console.log(studentObject);
+
+    newStuObject.fullname = stuData.fullname;
+    newStuObject.firstname = stuData.fullname.slice(0, firstSpace);
+    newStuObject.lastname = stuData.fullname.slice(lastSpace + 1);
+    newStuObject.house = stuData.house;
+    newStuObject.image =
+      "images/" +
+      newStuObject.lastname.toLowerCase() +
+      "_" +
+      stuData.fullname.substring(0, 1).toLowerCase() +
+      ".png";
+
+    //console.log(newStuObject.image);
+    arrayOfStudents.push(newStuObject);
+    console.log(arrayOfStudents);
+  });
 }
 
 function filterList() {
   console.log("filterList");
-
-  displayList();
 }
-
-function displayList() {
+displayList();
+function displayList(arrayOfStudents) {
   console.log("displayList");
 }
 
 // TODO: Create scaffolding functions for the rest!
 
-function clickSortByFirst() {
+function clickSortByFirst() {}
 
-}
-
-function sortListByFirst() {
-
-}
+function sortListByFirst() {}
